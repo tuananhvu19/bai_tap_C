e509e759e729710eb1247d115d1b4a022af55ca5
tuananhvu19 <142554346+tuananhvu19@users.noreply.github.com>
2023-08-22 15:25:08 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
-    cout << "This is my lesson about Git and Github";
+    cout << "This is my lesson about Git and Github\n";
+    cout << "dajkvghwag";
     return 0;
 }
